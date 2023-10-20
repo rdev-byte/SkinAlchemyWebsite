15b882b9d0fe755b5cd6ebe956d9ef74f0961800
RDevByte <rdev.byte@gmail.com>
2023-10-19 21:00:40 -0600

    Updated index, app css and js.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="Maharlika-Font">Welcome to Skin Alchemy</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Skin Alchemy is the passionate endeavor of Megan Rizk, a dedicated esthetician with over 11 years of experience in the beauty and skincare industry.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Megan's mission is to enhance your natural beauty and skin health through a combination of innovative skincare practices and the use of the finest skincare products.
+        </p>
+        <p>
+          With a focus on individualized care, Megan is committed to providing you with a revitalizing and personalized skincare experience.
+        </p>
+        <p>
+          Her expertise covers a wide range of skincare services, making Skin Alchemy your go-to destination for achieving radiant and healthy skin.
+        </p>
+
       </header>
     </div>
   );
