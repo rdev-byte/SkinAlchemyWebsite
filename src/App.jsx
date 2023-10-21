da760859ae1c6962128b1913723474bf358c75f5
RDevByte <rdev.byte@gmail.com>
2023-10-21 00:03:48 -0600

    updated css, fonts, and app

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,25 @@ import logo from './img/logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <span className="Welcome-Font">- Welcome to -</span>
-        <img src={logo} className="App-logo Logo" alt="logo" />
-        <p>
-          Skin Alchemy is the passionate endeavor of Megan Rizk, a dedicated esthetician with over 11 years of experience in the beauty and skincare industry.
-        </p>
-        <p>
-          Megan's mission is to enhance your natural beauty and skin health through a combination of innovative skincare practices and the use of the finest skincare products.
-        </p>
-        <p>
-          With a focus on individualized care, Megan is committed to providing you with a revitalizing and personalized skincare experience.
-        </p>
-        <p>
-          Her expertise covers a wide range of skincare services, making Skin Alchemy your go-to destination for achieving radiant and healthy skin.
-        </p>
-      </header>
+    <div className="Page-Wrapper">
+      <div className="App">
+          <span className="Welcome-Font">- Welcome to -</span>
+        <header className="App-header">
+          <img src={logo} className="Logo" alt="logo" />
+          <p>
+            Skin ALCHEMY is the passionate endeavor of Megan Rizk, a dedicated esthetician with over 11 years of experience in the beauty and skincare industry.
+          </p>
+          <p>
+            Megan's mission is to enhance your natural beauty and skin health through a combination of innovative skincare practices and the use of the finest skincare products.
+          </p>
+          <p>
+            With a focus on individualized care, Megan is committed to providing you with a revitalizing and personalized skincare experience.
+          </p>
+          <p>
+            Her expertise covers a wide range of skincare services, making Skin Alchemy your go-to destination for achieving radiant and healthy skin.
+          </p>
+        </header>
+      </div>
     </div>
   );
 }
