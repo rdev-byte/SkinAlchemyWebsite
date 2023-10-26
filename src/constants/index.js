b30b2d1c7f53a74df760bfef83fa2c43e03fa2bf
Joe Baldwin <124543001+JoeB007@users.noreply.github.com>
2023-10-26 01:37:12 -0500

    adding some css and playing around with layout

---
+++ src/constants/index.js
@@ -0,0 +1,32 @@
+export const sections  = [
+    {
+      id: 1,
+      section: 'About My Spa',
+      content: (
+        <>
+          <p>
+            Skin ALCHEMY ESTHETICS is the passionate endeavor of Megan Rizk, a dedicated esthetician with over 11 years of experience in the beauty and skincare industry.
+          </p>
+          <p>
+            Megan's mission is to enhance your natural beauty and skin health through a combination of innovative skincare practices and the use of the finest skincare products.
+          </p>
+          <p>
+            With a focus on individualized care, Megan is committed to providing you with a revitalizing and personalized skincare experience.
+          </p>
+          <p>
+            Her expertise covers a wide range of skincare services, making Skin Alchemy your go-to destination for achieving radiant and healthy skin.
+          </p>
+        </>
+      ),
+    },
+    {
+      id: 2,
+      section: 'Section 2',
+      content: null,
+    },
+    {
+      id: 3,
+      section: 'Section 3',
+      content: null,
+    },
+  ];
